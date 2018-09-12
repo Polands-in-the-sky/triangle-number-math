e08502b23a2f7380e6b2f85c1cbdd188cdc8ec62
Polands in the Sky <glugglugglug@vivaldi.net>
2018-09-12 19:32:40 +0800

    Documentation and query implementation

---
+++ README.md
@@ -0,0 +1,3 @@
+## Triangle-number-math
+
+This is a little program that automatically grabs a list of triangle numbers and says if they are multiples of a specific integer mentioned in an input. I created this for some maths stuff and learning Python in general.
--- Triangle_numbers.py
+++ Triangle_numbers.py
@@ -1,12 +1,9 @@
 def all_triangle_numbers(n):
     Query = int(input("What multiple are you searching for? "))
     for i in range(1, n + 1):
-        triangle = (i ** 2 + i)//2   
-        print("n = {0}, triangle = {1}".format(i, triangle))
+        triangle = (i ** 2 + i)//2
         if triangle % Query == 0:
-            print("This number is a multiple of {}! Woohoo!".format(Query))
-        else:
-            print("No, this number is not a multiple of {}.".format(Query))
+            print(triangle)
         
 
 all_triangle_numbers(50)    
