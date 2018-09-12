019e4a5816682eea947c903db361ad581a66f9d4
Polands in the Sky <glugglugglug@vivaldi.net>
2018-09-12 19:33:33 +0800

    Makes filename easier for me to run in shell

--- Triangle_numbers.py
+++
@@ -1,9 +0,0 @@
-def all_triangle_numbers(n):
-    Query = int(input("What multiple are you searching for? "))
-    for i in range(1, n + 1):
-        triangle = (i ** 2 + i)//2
-        if triangle % Query == 0:
-            print(triangle)
-        
-
-all_triangle_numbers(50)    
